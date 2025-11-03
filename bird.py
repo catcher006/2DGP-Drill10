dd8796807780562f8d527c5c150699d5f0efd558
catcher006 <seungho217@tukorea.ac.kr>
2025-11-03 14:03:21 +0900

    bird 기본 구조 설정

--- bird.py
+++ bird.py
@@ -3,31 +3,7 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 
 import game_world
 import game_framework
-from ball import Ball
 from state_machine import StateMachine
-
-
-def space_down(e): # e is space down ?
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
-
-time_out = lambda e: e[0] == 'TIMEOUT'
-
-def right_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
-
-
-def right_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
-
-
-def left_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
-
-
-def left_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
-
-
 
 # Boy의 Run Speed 계산
 
@@ -44,34 +20,7 @@ ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
 
-class Idle:
-
-    def __init__(self, boy):
-        self.boy = boy
-
-    def enter(self, e):
-        self.boy.wait_time = get_time()
-        self.boy.dir = 0
-
-
-    def exit(self, e):
-        if space_down(e):
-            self.boy.fire_ball()
-
-
-    def do(self):
-        self.boy.frame = (self.boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME *game_framework.frame_time) % 8
-        if get_time() - self.boy.wait_time > 3:
-            self.boy.state_machine.handle_state_event(('TIMEOUT', None))
-
-    def draw(self):
-        if self.boy.face_dir == 1: # right
-            self.boy.image.clip_draw(int(self.boy.frame) * 100, 300, 100, 100, self.boy.x, self.boy.y)
-        else: # face_dir == -1: # left
-            self.boy.image.clip_draw(int(self.boy.frame) * 100, 200, 100, 100, self.boy.x, self.boy.y)
-
-
-class Sleep:
+class Fly:
 
     def __init__(self, boy):
         self.boy = boy
@@ -97,37 +46,7 @@ class Sleep:
 
 
 
-class Run:
-    def __init__(self, boy):
-        self.boy = boy
-
-    def enter(self, e):
-        if right_down(e) or left_up(e):
-            self.boy.dir = self.boy.face_dir = 1
-        elif left_down(e) or right_up(e):
-            self.boy.dir = self.boy.face_dir = -1
-
-    def exit(self, e):
-        if space_down(e):
-            self.boy.fire_ball()
-
-    def do(self):
-        self.boy.frame = (self.boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME *game_framework.frame_time) % 8
-        self.boy.x += self.boy.dir * RUN_SPEED_PPS * game_framework.frame_time
-
-    def draw(self):
-        if self.boy.face_dir == 1: # right
-            self.boy.image.clip_draw(int(self.boy.frame) * 100, 100, 100, 100, self.boy.x, self.boy.y)
-        else: # face_dir == -1: # left
-            self.boy.image.clip_draw(int(self.boy.frame) * 100, 0, 100, 100, self.boy.x, self.boy.y)
-
-
-
-
-
-
-
-class Boy:
+class Bird:
     def __init__(self):
 
         self.item = None
@@ -139,17 +58,8 @@ class Boy:
         self.image = load_image('animation_sheet.png')
         self.font = load_font('ENCR10B.TTF', 16)
 
-        self.IDLE = Idle(self)
-        self.SLEEP = Sleep(self)
-        self.RUN = Run(self)
-        self.state_machine = StateMachine(
-            self.IDLE,
-            {
-                self.SLEEP : {space_down: self.IDLE},
-                self.IDLE : {space_down: self.IDLE, time_out: self.SLEEP, right_down: self.RUN, left_down: self.RUN, right_up: self.RUN, left_up: self.RUN},
-                self.RUN : {space_down: self.RUN, right_up: self.IDLE, left_up: self.IDLE, right_down: self.IDLE, left_down: self.IDLE}
-            }
-        )
+        self.FLY = Fly(self)
+        self.state_machine = StateMachine(self.FLY)
 
 
 
